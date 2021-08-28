e6fb0ae942e2a14a3f71fbd92831543a8525ce20
Arthaud Proust <arthaudp33@gmail.com>
2021-08-29 01:03:33 +0200

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     // mode: 'jit',
-    purge: [
-        './public/**/*.html',
-        './src/**/*.{js,jsx,ts,tsx,vue}',
-    ],
+    // purge: [
+    //     './public/**/*.html',
+    //     './src/**/*.{js,jsx,ts,tsx,vue}',
+    // ],
+    purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
